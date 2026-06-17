ebab497584448f19e0a855b1ef8f72c10fec30be
ngodoi-ads <ngodoi.k3@gmail.com>
2026-06-16 22:03:15 -0300

    Create ControleEstoquePadaria.java
    Este commit adiciona o exercício ControleEstoquePadaria.java na pasta fundamentos.
    O programa inicializa variáveis de farinha e açúcar e imprime os valores no console.

---
+++ fundamentos/ControleEstoquePadaria.java
@@ -0,0 +1,9 @@
+public class ControleEstoquePadaria {
+    public static void main (String[] args) {
+        int quantidadeFarinha = 10;
+        int quantidadeAcucar = 5;
+
+        System.out.println("Quantidade de farinha disponível: " + quantidadeFarinha + " xícaras");
+        System.out.println("Quantidade de açúcar disponível: " + quantidadeAcucar + " colheres de sopa");
+    }
+}
