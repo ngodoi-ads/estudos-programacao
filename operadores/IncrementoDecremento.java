146632dacb8ae26ac67f1cfb41c174eb44a12ced
ngodoi-ads <ngodoi.k3@gmail.com>
2026-06-16 22:50:58 -0300

    Adiciona exercício IncrementoDecremento.java
    Programa que demonstra operadores de incremento e decremento em Java.

---
+++ operadores/IncrementoDecremento.java
@@ -0,0 +1,13 @@
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        int contador = 5;
+
+        // Incremento
+        contador++; // Equivalente a contador = contador + 1;
+
+        // Decremento
+        contador--; // Equivalente a contador = contador - 1;
+
+        System.out.println("Valor final do contador: " + contador);
+    }
+}
