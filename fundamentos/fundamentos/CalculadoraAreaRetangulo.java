0bb0f84a5f18f857375382328d192005a38ec8fc
ngodoi-ads <ngodoi.k3@gmail.com>
2026-06-16 22:19:52 -0300

    Create CalculadoraAreaRetangulo.java
    Programa que calcula a área de um retângulo usando largura e altura.

---
+++ fundamentos/fundamentos/CalculadoraAreaRetangulo.java
@@ -0,0 +1,11 @@
+public class CalculadoraAreaRetangulo {
+    public static void main(String[] args) {
+        double largura = 5.0;
+        double altura = 3.0;
+        double area = largura * altura;
+
+        System.out.println("Largura do retângulo: " + largura);
+        System.out.println("Altura do retângulo: " + altura);
+        System.out.println("Área do retângulo: " + area);
+    }
+}
