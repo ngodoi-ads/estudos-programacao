454af5b82333c47b10e54e74919e2a495d1855d7
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-21 00:03:36 -0300

    Create ContagemDeDoces.java
    O programa utiliza um laço de repetição while para realizar uma contagem de 1 até 3. A cada repetição, o valor do contador de doces é exibido na tela e incrementado em 1 até atingir a condição de parada.

---
+++ loops/ContagemDeDoces.java
@@ -0,0 +1,19 @@
+public class ContagemDeDoces {
+
+public static void main(String[] args){
+
+int contadorDoces=1;
+
+
+while(contadorDoces<=3){
+
+System.out.println(contadorDoces);
+
+contadorDoces++;
+
+}
+
+
+}
+
+}
