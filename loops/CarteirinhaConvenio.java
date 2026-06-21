e2d29f361f62ae34d0d9460eb465a1c05f16a62f
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-21 00:07:18 -0300

    CarteirinhaConvenio.java
    A função do programa é simular a validação de códigos em uma carteirinha de convênio.

---
+++ loops/CarteirinhaConvenio.java
@@ -0,0 +1,38 @@
+public class CarteirinhaConvenio {
+
+
+public static void main(String[] args){
+
+
+int codigoDeSaida=9;
+
+
+for(int codigo=1;codigo<=10;codigo++){
+
+
+if(codigo==codigoDeSaida){
+
+System.out.println("Saindo...");
+break;
+
+}
+
+
+if(codigo==3 || codigo==7 || codigo==10){
+
+System.out.println("Aceito");
+
+continue;
+
+}
+
+
+System.out.println("Não aceito");
+
+
+}
+
+
+}
+
+}
