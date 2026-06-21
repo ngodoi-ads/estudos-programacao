79ee609f76d09995c01e7e85e739efe08574f798
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-20 23:17:41 -0300

    Adiciona exercício ContagemNumeros.java

---
+++ estruturas-repeticao/ContagemNumeros.java
@@ -0,0 +1,8 @@
+public class ContagemNumeros {
+    public static void main(String[] args) {
+        // Exemplo de contagem de 1 a 10 usando for
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Número: " + i);
+        }
+    }
+}
