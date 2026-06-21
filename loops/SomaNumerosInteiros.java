5b2225adc6e12273084681899c6b2114dc90006b
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-21 00:04:35 -0300

    SomaNumerosInteiros.java
    O programa utiliza o laço de repetição do while para somar números inteiros de 10 até 1. A variável numero começa em 10, é adicionada à variável soma e reduzida a cada repetição até chegar a 0. Ao final, exibe a soma total, que é 55.

---
+++ loops/SomaNumerosInteiros.java
@@ -0,0 +1,27 @@
+public class SomaNumerosInteiros {
+
+
+public static void main(String[] args){
+
+
+int numero=10;
+
+int soma=0;
+
+
+do{
+
+soma+=numero;
+
+numero--;
+
+
+}while(numero>0);
+
+
+System.out.println(soma);
+
+
+}
+
+}
