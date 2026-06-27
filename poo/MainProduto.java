69e6b6ca94db0e34f1690dd01f476cfe790acc24
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 13:57:23 -0300

    MainProduto.java

---
+++ poo/MainProduto.java
@@ -0,0 +1,23 @@
+public class MainProduto {
+
+
+    public static void main(String[] args){
+
+
+        Produto produto1 =
+        new Produto("Notebook",2500.00,10);
+
+
+        Produto produto2 =
+        new Produto("Smartphone",1500.00,20);
+
+
+
+        produto1.exibirInformacoes();
+
+        produto2.exibirInformacoes();
+
+
+    }
+
+}
