411a0eefc1100fcd9afaafb0d987bb8c27947b59
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 13:29:55 -0300

    Update CarteirinhaConvenio.java

--- loops/CarteirinhaConvenio.java
+++ loops/CarteirinhaConvenio.java
@@ -1,38 +1,26 @@
 public class CarteirinhaConvenio {
-
-
 public static void main(String[] args){
-
 
 int codigoDeSaida=9;
 
-
-for(int codigo=1;codigo<=10;codigo++){
-
+for(int codigo=1;codigo<=10;codigo++) {
 
 if(codigo==codigoDeSaida){
 
 System.out.println("Saindo...");
 break;
-
 }
-
 
 if(codigo==3 || codigo==7 || codigo==10){
-
+  
 System.out.println("Aceito");
-
 continue;
-
 }
-
-
 System.out.println("Não aceito");
 
+        }
 
-}
 
-
-}
+    } 
 
 }
