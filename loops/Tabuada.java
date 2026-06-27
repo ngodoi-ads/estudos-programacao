da7720d1c7b01375b130522725c596d1f865f8d6
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 13:17:30 -0300

    Tabuada.java
    Este programa em Java gera a tabuada de um número informado utilizando uma estrutura de repetição for.
    
    O código percorre os valores de 1 até 10 e realiza a multiplicação do número escolhido pelo contador, exibindo cada resultado no console

---
+++ loops/Tabuada.java
@@ -0,0 +1,22 @@
+public class Tabuada {
+
+    public static void main(String[] args) {
+
+
+        int numero = 5;
+
+
+        for(int i = 1; i <= 10; i++){
+
+
+            int resultado = numero * i;
+
+
+            System.out.println(
+                numero + " x " + i + " = " + resultado
+            );
+
+        }
+
+    }
+}
