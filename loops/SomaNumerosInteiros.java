96c2d8c471c39f26280bd8f1d20f79168e66c13c
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 13:31:16 -0300

    Update SomaNumerosInteiros.java

--- loops/SomaNumerosInteiros.java
+++ loops/SomaNumerosInteiros.java
@@ -1,16 +1,16 @@
 public class SomaNumerosInteiros {
-public static void main(String[] args){
+public static void main(String[] args) {
 
 int numero=10;
 int soma=0;
 
-do{
+do {
 
 soma+=numero;
 
 numero--;
 
-}while(numero>0);
+} while (numero>0);
 
 System.out.println(soma);
  
