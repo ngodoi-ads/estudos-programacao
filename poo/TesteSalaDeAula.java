ee6ce9e754e39bf7f355a81fa0621d2d17ff589f
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 19:00:40 -0300

    TesteSalaDeAula.java

---
+++ poo/TesteSalaDeAula.java
@@ -0,0 +1,17 @@
+public class TesteSalaDeAula {
+
+    public static void main(String[] args){
+
+        SalaDeAula sala1 = new SalaDeAula();
+        SalaDeAula sala2 = new SalaDeAula();
+        SalaDeAula sala3 = new SalaDeAula();
+
+        SalaDeAula.adicionarAluno();
+        SalaDeAula.adicionarAluno();
+        SalaDeAula.adicionarAluno();
+
+        SalaDeAula.exibirTotalAlunos();
+
+    }
+
+}
