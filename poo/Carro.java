92e47bad8e01036154118305de3e162458543fe8
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 13:57:50 -0300

    Carro.java

--- poo/Carro.java
+++ poo/Carro.java
@@ -40,27 +40,3 @@ public class Carro {
     }
 
 }
-public class Aplicacao {
-
-    public static void main(String[] args){
-
-
-        Carro carro1 = new Carro();
-
-        Carro carro2 =
-        new Carro("Toyota","Corolla");
-
-
-        Carro carro3 =
-        new Carro("Honda","Civic",2025);
-
-
-        carro1.exibirDetalhes();
-
-        carro2.exibirDetalhes();
-
-        carro3.exibirDetalhes();
-
-    }
-
-}
