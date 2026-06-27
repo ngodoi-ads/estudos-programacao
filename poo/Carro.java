15ba26c3607da21622c429e4a89088030ee6151e
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 13:33:41 -0300

    Carro.java
    Este programa demonstra os conceitos iniciais de Programação Orientada a Objetos em Java, criando uma classe Carro com atributos e métodos.
    
    A classe representa um carro com informações como marca, modelo e ano. O programa utiliza diferentes construtores para criar objetos com diferentes níveis de informações.

---
+++ poo/Carro.java
@@ -0,0 +1,47 @@
+public class Carro {
+
+    String marca;
+    String modelo;
+    int ano;
+
+
+    // Construtor padrão
+    public Carro(){
+
+        marca = "Desconhecida";
+        modelo = "Desconhecido";
+        ano = 0;
+
+    }
+
+
+    // Construtor com dois parâmetros
+    public Carro(String marca, String modelo){
+
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = 2024;
+
+    }
+
+
+    // Construtor com três parâmetros
+    public Carro(String marca, String modelo, int ano){
+
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+
+    }
+
+
+    public void exibirDetalhes(){
+
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+
+    }
+
+
+}
