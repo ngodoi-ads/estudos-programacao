711d586c2041cfd3c74f61310c0f4dc12c1056f7
Nathália de Godoi <ngodoi.k3@gmail.com>
2026-06-27 18:59:28 -0300

    SalaDeAula.java
    Este programa demonstra o uso de variáveis e métodos estáticos (static) em Java.
    
    A classe SalaDeAula possui uma variável estática que armazena o total de alunos. Como ela pertence à classe, seu valor é compartilhado entre todos os objetos criados.
    
    O programa utiliza métodos estáticos para adicionar alunos e exibir a quantidade total.
    
    Conceitos praticados:
    
    ✅ Classe e objetos
    ✅ Modificador static
    ✅ Variável compartilhada entre objetos
    ✅ Métodos estáticos
    ✅ Contador usando incremento

---
+++ poo/SalaDeAula.java
@@ -0,0 +1,26 @@
+public class SalaDeAula {
+
+
+    static int totalAlunos = 0;
+
+
+
+    public static void adicionarAluno(){
+
+        totalAlunos++;
+
+    }
+
+
+
+    public static void exibirTotalAlunos(){
+
+        System.out.println(
+        "Total de alunos na sala: " 
+        + totalAlunos
+        );
+
+    }
+
+
+}
